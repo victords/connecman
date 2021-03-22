a552755bc3f3d4bff8513d05608c1de318306192
Victor David Santos <victordavidsantos@gmail.com>
2021-03-22 09:21:07 -0300

    About, hall of fame and credits menus (in progress).

--- menu.rb
+++ menu.rb
@@ -11,6 +11,7 @@ class Menu
           set_state :play
         },
         Button.new(90, 265, ConnecMan.default_font, ConnecMan.text(:instructions), :main_btn2, 0x333300, 0, 0x666633, 0x333300, false, true, 8) {
+          set_paging(:instructions, :main, 9)
           set_state :instructions
         },
         Button.new(90, 305, ConnecMan.default_font, ConnecMan.text(:options), :main_btn2, 0x333300, 0, 0x666633, 0x333300, false, true, 8) {
@@ -28,22 +29,51 @@ class Menu
           ConnecMan.new_game
         },
         Button.new(325, 300, ConnecMan.default_font, ConnecMan.text(:continue), :main_btn3, 0xffffff, 0, 0xffff00, 0xff8000) {
+          set_continue_buttons
           set_state :continue
         },
         Button.new(325, 360, ConnecMan.default_font, ConnecMan.text(:back), :main_btn3, 0xffffff, 0, 0xffff00, 0xff8000) {
           set_state :main
         }
+      ],
+      more: [
+        Button.new(325, 220, ConnecMan.default_font, ConnecMan.text(:about), :main_btn3, 0xffffff, 0,0xffff00, 0xff8000) {
+          set_paging(:about, :more, 3)
+          set_state :about
+        },
+        Button.new(325, 280, ConnecMan.default_font, ConnecMan.text(:hall_of_fame), :main_btn3, 0xffffff, 0,0xffff00, 0xff8000) {
+          set_state :hall_of_fame
+        },
+        Button.new(325, 340, ConnecMan.default_font, ConnecMan.text(:credits), :main_btn3, 0xffffff, 0,0xffff00, 0xff8000) {
+          set_state :credits
+        },
+        Button.new(325, 400, ConnecMan.default_font, ConnecMan.text(:back), :main_btn3, 0xffffff, 0,0xffff00, 0xff8000) {
+          set_state :main
+        },
+      ],
+      hall_of_fame: [
+        Button.new(325, 450, ConnecMan.default_font, ConnecMan.text(:back), :main_btn3, 0xffffff, 0,0xffff00, 0xff8000) {
+          set_state :more
+        },
+      ],
+      credits: [
+        Button.new(325, 450, ConnecMan.default_font, ConnecMan.text(:back), :main_btn3, 0xffffff, 0,0xffff00, 0xff8000) {
+          set_state :more
+        },
       ]
     }
 
     @saves = []
+    hall_of_fame = []
     save_files = Dir["#{ConnecMan.saves_path}/*"]
     save_files.each do |s|
-      @saves << s.split('/')[-1]
-      set_continue_buttons
+      name = s.split('/')[-1]
+      @saves << name
+      data = File.read(s).split('#', -1)
+      total_score = data[2].split(',').map(&:to_i).sum
+      hall_of_fame << { name: name, score: total_score }
     end
-
-    set_instructions_buttons
+    @hall_of_fame = hall_of_fame[0..9].sort! { |a, b| b[:score] <=> a[:score] }
 
     @board1 = Res.img(:main_Board)
     @board2 = Res.img(:main_Board2)
@@ -56,7 +86,6 @@ class Menu
   def set_state(state, play_sound = true)
     @state = state
     @title = ConnecMan.text(state)
-    @page_index = 0
     ConnecMan.play_sound('1') if play_sound
   end
 
@@ -85,20 +114,20 @@ class Menu
     }
   end
 
-  def set_instructions_buttons(page = 0)
+  def set_paging(state, prev_state, page_count, page = 0)
     @page_index = page
-    @controls[:instructions] = []
+    @controls[state] = []
     if page > 0
-      @controls[:instructions] << Button.new(155, 450, ConnecMan.default_font, ConnecMan.text(:previous), :main_btn3, 0xffffff, 0, 0xffff00, 0xff8000) {
-        set_instructions_buttons(@page_index - 1)
+      @controls[state] << Button.new(155, 450, ConnecMan.default_font, ConnecMan.text(:previous), :main_btn3, 0xffffff, 0, 0xffff00, 0xff8000) {
+        set_paging(state, prev_state, page_count, @page_index - 1)
       }
     end
-    @controls[:instructions] << Button.new(325, 450, ConnecMan.default_font, ConnecMan.text(:back), :main_btn3, 0xffffff, 0, 0xffff00, 0xff8000) {
-      set_state :main
+    @controls[state] << Button.new(325, 450, ConnecMan.default_font, ConnecMan.text(:back), :main_btn3, 0xffffff, 0, 0xffff00, 0xff8000) {
+      set_state prev_state
     }
-    if page < 8
-      @controls[:instructions] << Button.new(495, 450, ConnecMan.default_font, ConnecMan.text(:next), :main_btn3, 0xffffff, 0, 0xffff00, 0xff8000) {
-        set_instructions_buttons(@page_index + 1)
+    if page < page_count - 1
+      @controls[state] << Button.new(495, 450, ConnecMan.default_font, ConnecMan.text(:next), :main_btn3, 0xffffff, 0, 0xffff00, 0xff8000) {
+        set_paging(state, prev_state, page_count, @page_index + 1)
       }
     end
   end
@@ -113,9 +142,9 @@ class Menu
     @controls[:main].each(&:draw)
 
     board = case @state
-            when :play, :continue
+            when :play, :continue, :more
               @board1
-            when :instructions
+            when :instructions, :about, :hall_of_fame, :credits
               @board2
             end
     if board
@@ -130,6 +159,13 @@ class Menu
         @instructions_images[@page_index].draw(120, 330, 0)
       elsif @page_index == 5
         @instructions_images[@page_index + ConnecMan.langs.index(ConnecMan.language)].draw(120, 330, 0)
+      end
+    elsif @state == :about
+      ConnecMan.text_helper.write_breaking(ConnecMan.text("about_#{@page_index}"), 120, 190, 560, :justified, 0xffffff)
+    elsif @state == :hall_of_fame
+      @hall_of_fame.each_with_index do |h, i|
+        ConnecMan.text_helper.write_line(h[:name], 180, 155 + i * 30, :left, 0xffffff)
+        ConnecMan.text_helper.write_line(h[:score].to_s, 620, 155 + i * 30, :right, 0xffffff)
       end
     end
 
