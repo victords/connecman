4869fead31ec1f76b74318b7aa23931deeef602b
Victor David Santos <victordavidsantos@gmail.com>
2022-04-03 10:16:53 -0300

    Package for v1.1.1.

--- menu.rb
+++ menu.rb
@@ -202,6 +202,8 @@ class Menu < Controller
     else
       @controls[@state].each(&:draw) if @state != :main
     end
+    
+    ConnecMan.text_helper.write_line('v1.1.1', 790, 570, :right)
 
     super
   end
--- package.sh
+++ package.sh
@@ -2,4 +2,4 @@
 
 cp *.rb deb/opt/vds-games/connecman/
 cp -r data deb/opt/vds-games/connecman/
-dpkg -b deb connecman-1.1.0.deb
+dpkg -b deb connecman-1.1.1.deb
